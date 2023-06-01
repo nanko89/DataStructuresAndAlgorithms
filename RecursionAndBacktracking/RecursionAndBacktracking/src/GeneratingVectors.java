8239f774c8dc088bb2cebb68464007ea5b56d238
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-01 15:17:03 +0300

    Add Generating Vectors

--- RecursionAndBacktracking/RecursionAndBacktracking/src/GeneratingVectors.java
+++ RecursionAndBacktracking/RecursionAndBacktracking/src/GeneratingVectors.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GeneratingVectors {
@@ -6,11 +7,27 @@ public class GeneratingVectors {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        int[] memory = new int[n];
+        Integer[] memory = new Integer[n];
         
-        fillVector(memory);
+        fillVector(memory, 0);
     }
 
-    private static void fillVector(int[] memory) {
+    private static void fillVector(Integer[] memory, int index) {
+        if (index == memory.length){
+           pintVector(memory);
+           return;
+        }
+
+        for (int i = 0; i <= 1 ; i++) {
+            memory[index]= i;
+            fillVector(memory, index + 1);
+        }
+    }
+
+    private static void pintVector(Integer[] memory) {
+        for (int i = 0; i < memory.length; i++) {
+            System.out.print(memory[i]);
+        }
+        System.out.println();
     }
 }
