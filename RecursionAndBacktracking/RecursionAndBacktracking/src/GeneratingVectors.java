2621c7130e5cc6f5ea89694d68a82edb4eceb004
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-01 14:20:41 +0300

    Add Recursive Fibonacii

--- RecursionAndBacktracking/RecursionAndBacktracking/src/GeneratingVectors.java
+++ RecursionAndBacktracking/RecursionAndBacktracking/src/GeneratingVectors.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class GeneratingVectors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] memory = new int[n];
+        
+        fillVector(memory);
+    }
+
+    private static void fillVector(int[] memory) {
+    }
 }
--- RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveFibonacci.java
+++ RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveFibonacci.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RecursiveFibonacci {
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        int first = 1;
+        int second = 1;
+
+        long result = fibonacciSum(first, second, num);
+
+        System.out.println(result);
+    }
+
+    private static long fibonacciSum(int first, int second, int number) {
+
+        if (number <= 1) {
+            return 1;
+        }
+        return first + fibonacciSum(second, first + second, number - 1);
+    }
 }
