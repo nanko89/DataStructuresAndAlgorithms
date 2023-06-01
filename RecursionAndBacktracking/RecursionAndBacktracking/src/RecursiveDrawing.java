923f1f70afdbcdaa028849bdfb80b50946db8370
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-01 14:19:51 +0300

    Add RecursionDrawing

--- RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveDrawing.java
+++ RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveDrawing.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class RecursiveDrawing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        printStars("*", number);
+        printHash("#", number ,1 );
+    }
+
+    private static void printHash(String s, int number, int count) {
+        if (number < count){
+            return;
+        }
+        System.out.print(s.repeat(count));
+        System.out.println();
+        printHash(s, number, count + 1 );
+    }
+
+    private static void printStars(String s, int number) {
+        if (number == 0){
+            return;
+        }
+        System.out.print(s.repeat(number));
+        System.out.println();
+        printStars(s, number-1);
+    }
 }
