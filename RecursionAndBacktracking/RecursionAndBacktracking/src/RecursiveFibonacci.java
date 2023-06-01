778d44325f3da082086cef05d584b432948c80f9
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-01 14:27:49 +0300

    Update Recursive Fibonacci

--- RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveFibonacci.java
+++ RecursionAndBacktracking/RecursionAndBacktracking/src/RecursiveFibonacci.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
 public class RecursiveFibonacci {
+    private static long[] memoNums;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int num = Integer.parseInt(scanner.nextLine());
 
-        int first = 1;
-        int second = 1;
+        memoNums = new long[num + 1];
 
-        long result = fibonacciSum(first, second, num);
-
-        System.out.println(result);
+        System.out.println(recursiveFibonacci(num));
     }
 
-    private static long fibonacciSum(int first, int second, int number) {
-
-        if (number <= 1) {
+    private static long recursiveFibonacci(int n) {
+        if (n <= 1) {
             return 1;
         }
-        return first + fibonacciSum(second, first + second, number - 1);
+
+        if (memoNums[n] != 0) {
+            return memoNums[n];
+        }
+
+        memoNums[n] =
+                recursiveFibonacci(n - 1) +
+                        recursiveFibonacci(n - 2);
+
+        return memoNums[n];
     }
 }
