282b341570ac7b2c9c9d27e64620eef70b1c691b
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-02 15:46:58 +0300

    Add Combinations with Repetition Exercise

--- CombinatorialProblems/src/CombinationsWithoutRepetition.java
+++ CombinatorialProblems/src/CombinationsWithoutRepetition.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class CombinationsWithoutRepetition {
+    public static String[] symbols;
+    public static String[] combinations;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        symbols = scanner.nextLine().split("\\s+");
+        int spots = Integer.parseInt(scanner.nextLine());
+
+        combinations = new String[spots];
+        combination(0,0);
+
+    }
+
+    public static void combination(int index, int start){
+        if (index == combinations.length){
+            print();
+        }else {
+            for (int i = start; i < symbols.length ; i++) {
+                combinations[index] = symbols[i];
+                combination(index + 1 , i +1);
+            }
+        }
+
+    }
+
+    private static void print() {
+        System.out.println(String.join(" ", combinations));
+    }
 }
