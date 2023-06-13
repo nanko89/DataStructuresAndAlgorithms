2c3dc0d6f79a8af51d16f159bf6e94adfe8766a9
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-13 16:47:43 +0300

    Update exercise

--- GraphTheoryTraversalAndShortestPaths/src/main/java/Main.java
+++ GraphTheoryTraversalAndShortestPaths/src/main/java/Main.java
@@ -57,6 +57,45 @@ public class Main {
 
 
     public static Collection<String> topSort(Map<String, List<String>> graph) {
-        throw new AssertionError("Not Implemented");
+        Map<String, Integer> dependenciesCount = getDependenciesCount(graph);
+
+        List<String> sorted = new ArrayList<>();
+
+        while(!graph.isEmpty()){
+            String current = graph.keySet().stream().filter(k -> dependenciesCount.get(k) == 0)
+                    .findFirst()
+                    .orElse(null);
+
+            if (current == null){
+                break;
+            }
+
+            graph.get(current).forEach( c -> dependenciesCount.put(c, dependenciesCount.get(c) - 1));
+
+            sorted.add(current);
+            graph.remove(current);
+        }
+
+        if (!graph.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+
+        return sorted;
+    }
+
+    private static Map<String, Integer> getDependenciesCount(Map<String, List<String>> graph) {
+
+        Map<String, Integer> dependenciesCount = new LinkedHashMap<>();
+
+
+        for (Map.Entry<String, List<String>> node : graph.entrySet()) {
+            dependenciesCount.putIfAbsent(node.getKey(), 0);
+            for (String child : node.getValue()){
+                dependenciesCount.putIfAbsent(child, 0);
+                dependenciesCount.put(child, dependenciesCount.get(child) +1);
+            }
+        }
+
+        return dependenciesCount;
     }
 }
--- GraphTheoryTraversalAndShortestPaths/src/main/java/ShortestPath.java
+++ GraphTheoryTraversalAndShortestPaths/src/main/java/ShortestPath.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class ShortestPath {
+import java.util.*;
+
+public class ShortestPath {
+    public static boolean[] visited;
+    private static int[] prevNodes;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int edges = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < edges; i++) {
+            int[] paths = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+            graph.get(paths[0]).add(paths[1]);
+        }
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+
+        visited = new boolean[n + 1];
+        prevNodes = new int[n + 1];
+
+        Arrays.fill(prevNodes, -1);
+
+        bfs(graph, start, end);
+
+        List<Integer> path = new ArrayList<>();
+        path.add(end);
+
+        int prevNode = prevNodes[end];
+
+        while (prevNode != -1){
+            path.add(prevNode);
+            prevNode = prevNodes[prevNode];
+        }
+
+        System.out.println("Shortest path length is: " + (path.size() -1 ));
+
+        for (int i = path.size() - 1; i >=0  ; i--) {
+            System.out.print(path.get(i) + " ");
+        }
+    }
+
+    private static void bfs(List<List<Integer>> graph, int start, int end) {
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            if (node == end) {
+                System.out.println();
+                return;
+            }
+
+            for (int child : graph.get(node)) {
+                if (!visited[child]) {
+                    visited[child] = true;
+                    prevNodes[child] = node;
+                    queue.offer(child);
+                }
+            }
+        }
+    }
 }
