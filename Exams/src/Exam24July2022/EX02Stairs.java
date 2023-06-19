e02f40adebbb7ce7c4a692ee8c37e497d418e0b5
Natali Angelova <natali.v.angelova@gmail.com>
2023-06-19 15:08:01 +0300

    Add Stairs exercise and Guards

--- Exams/src/Exam24July2022/EX02Stairs.java
+++ Exams/src/Exam24July2022/EX02Stairs.java
@@ -1,4 +1,35 @@
 package Exam24July2022;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class EX02Stairs {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        long[] dp = new long[n +1];
+
+        Arrays.fill(dp, -1);
+
+        fib(n, dp);
+
+        System.out.println(dp[n]);
+
+    }
+
+    private static long fib(int n, long[] dp) {
+        if (n <= 1){
+            return dp[n] = 1;
+        }
+        if (dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = fib(n-1, dp) + fib(n-2, dp);
+    }
+
 }
--- Exams/src/Exam24July2022/Ex03Guards.java
+++ Exams/src/Exam24July2022/Ex03Guards.java
@@ -1,4 +1,54 @@
 package Exam24July2022;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class Ex03Guards {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int nodes = Integer.parseInt(reader.readLine());
+        int edges = Integer.parseInt(reader.readLine());
+
+        int[][] dp = new int[nodes + 1][nodes + 1];
+        boolean[] visited = new boolean[nodes + 1];
+
+        for (int i = 0; i < edges; i++) {
+            int[] connections = Arrays.stream(reader.readLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            dp[connections[0]][connections[1]] = 1;
+        }
+
+
+        int startIndex = Integer.parseInt(reader.readLine());
+
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(startIndex);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            if (!visited[node]) {
+                visited[node] = true;
+
+                for (int i = 0; i < dp[node].length; i++) {
+                    if (dp[node][i] == 1) {
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < visited.length; i++) {
+            if (!visited[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
